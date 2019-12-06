af45f68dd85ee1c9eb40406873d3f00880a019be
JD-Fuller <jdfuller@live.com>
2019-12-06 16:21:08 -0700

    cleanup

--- app/controllers/image-controller.js
+++ app/controllers/image-controller.js
@@ -2,14 +2,9 @@ import ImageService from "../services/image-service.js";
 import Image from "../models/image.js";
 import store from "../store.js";
 
-//TODO Create methods for constructor, and rendering the image to the page
-//      (you may wish to set it as a background image)
 function drawImage() {
   let i = store.State.image;
-  // document.getElementById("image").innerHTML = i.Template;
-
   document.body.style.background = i.Template;
-
   console.log("The image man says", store.State.image);
 }
 

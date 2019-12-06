1e0431ce55b4337bd75f55b9429e610409b7060b
JD-Fuller <jdfuller@live.com>
2019-12-06 15:41:07 -0700

    weather and quote upload

--- app/services/quote-service.js
+++ app/services/quote-service.js
@@ -1,11 +1,18 @@
+import Quote from "../models/quote.js";
+import store from "../store.js";
+
 // @ts-ignore
 const _quoteApi = axios.create({
   baseURL: "//bcw-sandbox.herokuapp.com/api/quotes",
   timeout: 3000
 });
-
-//TODO create methods to retrieve data trigger the update window when it is complete
-class QuoteService {}
+class QuoteService {
+  async getQuote() {
+    console.log("Calling the Quote-man");
+    let res = await _quoteApi.get();
+    store.commit("quote", new Quote(res.data.quote));
+  }
+}
 
 const quoteService = new QuoteService();
 export default quoteService;
